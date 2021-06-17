781e4399b82b14ec589e3657f34a2195084243c8
MING YAN TSAI <a976543257@gmail.com>
2021-06-17 10:25:52 +0800

    first commit

---
+++ install.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+# Setup script for setting up a new macos machine
+echo "Starting lastupdate"
+
+## Setup /etc/sudoers for sudo without password prompt
+echo "Setup NOPASSWD for %staff"
+sudo grep -q '^%staff' /etc/sudoers || sudo sed -i '' 's/^%admin.*/&\n%staff          ALL = (ALL) NOPASSWD: ALL/' /etc/sudoers
+
+## Command Line Tools for Xcode
+# echo "Install command line developer tools"
+# xcode-select --install
+# xcode-select -p &> /dev/null
+# if [ $? -ne 0 ]; then
+#   echo "Xcode CLI tools not found. Installing them..."
+#   touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+#   PROD=$(softwareupdate -l |
+#     grep "\*.*Command Line" |
+#     head -n 1 | awk -F"*" '{print $2}' |
+#     sed -e 's/^ *//' |
+#     tr -d '\n')
+#   softwareupdate -i "$PROD" -v;
+# else
+#   echo "Xcode CLI tools OK"
+# fi
+
+## Homebrew
+echo "Install Homebrew"
+CI=1; /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+## git
+brew install git
+git config --global user.email "a976543257@gmail.com"
+git config --global user.name "wheat"
+
+## Visual Studio Code
+brew install visual-studio-code
+
+code --install-extension hollowtree.vue-snippets
+
+
+# ## mas-cli
+# ## A simple command line interface for the Mac App Store. Designed for scripting and automation.
+# brew install mas
+
+# # Google Chrome
+# brew install google-chrome
+
+# # Line
+# mas install 539883307
+
+# # TeamViewer
+# brew install teamviewer
+
+# # AnyDesk
+# brew install anydesk
+
+# ## Zoom, Slack
+# brew install zoom slack
+
+# ## Sketch, Figma
+# brew install sketch
+
+# # Docker, Vagrant
+# brew install docker vagrant
+
+# ## Node.js
+# brew install fnm
